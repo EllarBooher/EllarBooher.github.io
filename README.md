9777363ac0711ced7146dfa1522657bd14d3824d
Lucas Booher <15761263+EllarBooher@users.noreply.github.com>
2024-07-18 16:46:34 -0700

    Initial website

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # EllarBooher.github.io
-This repository hosts a showcase of my portfolio projects.
+This repository hosts a showcase of my portfolio projects, hosted at https://ellarbooher.github.io/ via github pages.
+
+The npm package [gh-pages](https://www.npmjs.com/package/gh-pages) was used to help deploy.
 
 The source of the webpages lives on `main`, while the deployed build is on a separate branch `gh-pages`.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { memo } from 'react';
 import './App.css';
+
+const emailLink = <a target="_blank" className="hyperlink" href="mailto:estelle.booher@gmail.com">estelle.booher@gmail.com</a>;
+const githubLink = <a target="_blank" className="hyperlink" href="https://github.com/EllarBooher">https://github.com/EllarBooher</a>;
+
+const DisplayCard = memo(({
+  hyperlink, 
+  thumbnailAssets=[],
+  title=`PLACEHOLDER TITLE`, 
+  description=`PLACEHOLDER DESCRIPTION`
+}) => {
+  const thumbnails = <div className="DisplayCard-thumbnails">{
+    thumbnailAssets.map(e => 
+      <div className="DisplayCard-thumbnail">
+        <img className="DisplayCard-image" src={e}/>
+      </div>)
+  }</div>;
+
+  return (<a className="DisplayCard" href={hyperlink} target="_blank">
+    <div>
+      <div className="DisplayCard-name">
+        {title}
+      </div>
+      <div className="DisplayCard-body">
+        {description}
+        {thumbnails}
+      </div>
+    </div>
+  </a>);
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Estelle Booher
       </header>
+      <div className="App-main">
+        <div>
+          Hello, my name is Estelle Booher. 
+          I am working on building a portfolio and career in computer graphics, 
+          and my passion is realtime rendering and engine backend infrastructure. 
+          This website is a landing spot where I will host links to various projects of mine.
+          <br/>
+          <br/>
+          My github is at {githubLink}, where I host the source code of my projects including this website.
+          <br/>
+          To contact me, please email at {emailLink}.
+          <br/>
+          <h1>Graphics Programming</h1>
+          <DisplayCard 
+            hyperlink={`https://github.com/EllarBooher/Syzygy`} 
+            thumbnailAssets={[ require('./assets/syzygy1.png'), require('./assets/syzygy2.png') ]}
+            title={`Syzygy`} 
+            description={`
+              A sandbox renderer I started to study C++ and Vulkan. 
+              It aims to implement various features common in modern engines.
+            `}
+          />
+          <h1>Video Games</h1>
+          <DisplayCard 
+            hyperlink={`https://ellarbooher.itch.io/snail-blazer`}
+            thumbnailAssets={[ require('./assets/snailblazer1.png'), require('./assets/snailblazer2.png')]}
+            title={`Snail Blazer`}
+            description={`
+              A short bullet-hell video game made for the Bullet Hell Jam 2023 on itch.io. 
+              The player primarily moves via grappling on enemy projectiles and the environment, 
+              instead of the conventional WASD-style of movement.
+            `}
+          />
+        </div>
+      </div>
+      <footer className="App-footer">
+        All works present are copyrighted, unless externally provided with attributions or license.
+      </footer>
     </div>
   );
 }

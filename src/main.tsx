e8c73c548c5edc83d8455d5945a31a207c74b962
Estelle Booher <15761263+EllarBooher@users.noreply.github.com>
2025-01-23 18:12:05 -0800

    consistent indentation

--- src/main.tsx
+++ src/main.tsx
@@ -9,23 +9,23 @@ import { NavigationHeader } from './NavigateLink.tsx'
 const root = document.getElementById('root')!;
 
 createRoot(root).render(
-  <StrictMode>
-    <HashRouter>
-      <div style={{display:"flex", flexDirection:"column", height: '100vh'}}>
-        <div style={{flex: '0 1 auto'}}>  
-          <NavigationHeader/>
-        </div>
-          <Routes>
-            <Route index element={<App />} />
-            <Route path="webgpu-samples" element={
-              <div style={{flex: 1, overflow: 'hidden'}}>
-                <RendererComponent/>        
-              </div>
-            }/>
-            <Route path="*" element={<Navigate to="/" replace/>}/>
-          </Routes>
+	<StrictMode>
+		<HashRouter>
+			<div style={{display:"flex", flexDirection:"column", height: '100vh'}}>
+				<div style={{flex: '0 1 auto'}}>  
+					<NavigationHeader/>
+				</div>
+					<Routes>
+						<Route index element={<App />} />
+						<Route path="webgpu-samples" element={
+							<div style={{flex: 1, overflow: 'hidden'}}>
+								<RendererComponent/>        
+							</div>
+						}/>
+						<Route path="*" element={<Navigate to="/" replace/>}/>
+					</Routes>
 
-      </div>
-    </HashRouter>
-  </StrictMode>,
+			</div>
+		</HashRouter>
+	</StrictMode>,
 )

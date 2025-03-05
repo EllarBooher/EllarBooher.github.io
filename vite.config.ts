6cc69ff56036c6d80512141e18ba00196c3a4eb2
Estelle Booher <15761263+EllarBooher@users.noreply.github.com>
2025-03-05 14:47:02 -0800

    react to changing static .inc.wgsl files for hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, ViteDevServer } from "vite";
 import react from "@vitejs/plugin-react";
 import { TransformResult } from "rollup";
 import { packShaders } from "./src/shaders/Shaders";
@@ -19,7 +19,32 @@ const wgslPlugin = () => ({
 	},
 });
 
+const wgslIncludeWatcher = () => {
+	return {
+		name: "custom-watcher",
+		configureServer: (server: ViteDevServer) => {
+			const SUFFIX = ".inc.wgsl";
+
+			server.watcher.add("**/*" + SUFFIX);
+			function onWatchChange(_: string) {
+				server.hot.send({ type: "full-reload" });
+			}
+
+			const filterPath = (path: string) => {
+				if (!path.endsWith(SUFFIX)) {
+					return;
+				}
+				onWatchChange(path);
+			};
+
+			server.watcher.on("add", filterPath);
+			server.watcher.on("unlink", filterPath);
+			server.watcher.on("change", filterPath);
+		},
+	};
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [wgslPlugin(), react(), eslint()],
+	plugins: [wgslPlugin(), react(), eslint(), wgslIncludeWatcher()],
 });

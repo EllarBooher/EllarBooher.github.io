2fe6587ea9cb5607f3ef3063b65aa2cec1909372
Estelle Booher <15761263+EllarBooher@users.noreply.github.com>
2025-03-11 14:07:36 -0700

    use vite plugin checker instead of tsc, adds type checking to runtime and build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { wgslPlugin } from "./src/shaders/WGSLPlugin";
 import eslint from "@nabla/vite-plugin-eslint";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
 	plugins: [
@@ -10,6 +11,11 @@ export default defineConfig({
 		eslint({
 			eslintOptions: {
 				cacheLocation: "./node_modules/@eslint/.eslintcache",
+			},
+		}),
+		checker({
+			typescript: {
+				tsconfigPath: "./tsconfig.app.json",
 			},
 		}),
 	],

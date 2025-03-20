da81e3fc2e8ebb8ff247e8d200461b03b17b32c7
Estelle Booher <15761263+EllarBooher@users.noreply.github.com>
2025-03-19 18:25:44 -0700

    added SSL for https while dev testing on local server

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,25 @@ import { wgslPlugin } from "./src/shaders/WGSLPlugin";
 import eslint from "@nabla/vite-plugin-eslint";
 import checker from "vite-plugin-checker";
 import staticAsset from "./src/StaticAssetPlugin";
+import basicSSL from "@vitejs/plugin-basic-ssl";
 
-export default defineConfig({
-	plugins: [
-		wgslPlugin(),
-		staticAsset({ extensions: [".md"] }),
-		react(),
-		eslint({
-			eslintOptions: {
-				cacheLocation: "./node_modules/@eslint/.eslintcache",
-			},
-		}),
-		checker({
-			typescript: {
-				tsconfigPath: "./tsconfig.app.json",
-			},
-		}),
-	],
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [
+			wgslPlugin(),
+			staticAsset({ extensions: [".md"] }),
+			react(),
+			eslint({
+				eslintOptions: {
+					cacheLocation: "./node_modules/@eslint/.eslintcache",
+				},
+			}),
+			checker({
+				typescript: {
+					tsconfigPath: "./tsconfig.app.json",
+				},
+			}),
+			...(mode === "development" ? [basicSSL()] : []),
+		],
+	};
 });
